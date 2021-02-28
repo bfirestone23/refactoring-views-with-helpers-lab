bab3bf8374978da4bd65f687f9afd6530d95b8c0
Brian Firestone <bfirestone2339@gmail.com>
2021-02-28 18:43:11 -0500

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,12 @@
 module ArtistsHelper
+
+    def display_artist(song)
+        s = Song.find(song.id)
+        if s.artist.present?
+            link_to "#{s.artist.name}", artist_path(s.artist)
+        else
+            link_to "Add Artist", edit_song_path(s)
+        end
+    end
+
 end
